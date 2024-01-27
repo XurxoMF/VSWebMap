468ba97e68efa330154208ca8b38e49662451a25
Xurxo Martínez Ferreira <xurxolaxe@gmail.com>
2024-01-27 11:55:54 +0100

    1.0.0 release | Added rules page

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -9,6 +9,10 @@ export let pages = [
 	{
 		name: 'Discord',
 		route: '/discord'
+	},
+	{
+		name: 'Normas',
+		route: 'rules'
 	}
 ];
 
@@ -36,21 +40,23 @@ export let texts = {
 		</span>
 		<br />
 		<span class="inline-block mb-2">
-			Cualquiera puede entrar, esté o no en el servidor de Discord, desde el menú de servidores públicos dentro del juego pero, por estar en el servidor <a href="https://discord.gg/ybneJBRAWx" class="font-bold underline">Astro Vaporwave</a>, se tendrán algunas ventajas como canales privados para tu clan (grupos dentro del juego), soporte en caso de algún error, notificaciones de novedades y actualizaciones, etc.
+			Para poder entrar tendrás que acceder al <a href="https://discord.gg/ybneJBRAWx" class="font-bold underline">servidor de Discord Astro Vaporwave</a>, ir a <a href="https://discord.com/channels/726133117722820671/1173377109750726657" class="font-bold underline">este canal</a> y solicitar el acceso al servidor. A parte del acceso al servidor, también se tendrán algunas ventajas como canales privados para tu clan (grupos dentro del juego), soporte en caso de algún error, notificaciones de novedades y actualizaciones, etc.
 		</span>
 	`,
 	TitleServerFeaturesHTML: `CARACTERÍSTICAS DEL SERVIDOR`,
 	ListServerFeaturesHTML: [
 		'Abierto 24/7/365',
 		'32 jugadores simultáneos',
+		'Whitelist',
 		'PvE + PvP activado para defensa propia',
+		'Empiezas con 1 engranaje temporal',
 		'Anticheat por si las moscas',
 		'Resonance Archives bloqueado, será un evento del servidor',
-		'Con océanos, continentes y ríos con corriente (Rivers)',
+		'Con océanos, continentes y ríos con corriente',
 		'Mazmorras, estructuras y minas enormes',
 		'El fuego no se propaga',
-		'Amigable con jugadores nuevos en el servidor y en el juego en general',
-		'Economía y creación de tiendas(Vinconomy)',
+		'Amigable con jugadores nuevos en el servidor y en el juego',
+		'Economía y creación de tiendas',
 		'Claimeo de terrenos ilimitado',
 		'Creación de grupos/clanes/facciónes...',
 		'Chat de voz de proximidad',
@@ -61,14 +67,50 @@ export let texts = {
 	],
 	TitleWorldFeaturesHTML: `CARACTERÍSTICAS DEL MUNDO`,
 	ListWorldFeaturesHTML: [
-		'+70 Mods',
+		'+60 Mods',
 		'1.000.000 x 1.000.000 bloques',
 		'Caducidad de la comida 75%',
 		'Respawns infinitos con engranajes temporales',
 		'Casi todos los bloques son cincelables',
 		'12 días por mes',
 		'Tormentas temporales cada 10-20 días',
-		'60% tierra firme y 40% océanos, con una escala del 400%',
-		'Sin recetas exclusivas de clase'
+		'70% tierra firme y 30% océanos, con una escala del 400%'
+	],
+
+	/* Rules */
+	RulesTitle: 'Normas del servidor',
+	RulesDescription:
+		'El servidor tiene como objetivo ser divertido y amigable con todos. Para esto, es necesario que todos cumplamos algunas reglas básicas.<br/>Incumplir alguna de estas normas pude resultar en un baneo del servidor de Vintage Story e includo del Servidor de Discord.',
+	Rules: [
+		{
+			title: 'No reclamar recursos naturales',
+			description:
+				'No reclamar recursos naturales como minerales, árboles frutales(si los plantasteis vosotros si), etc...<br/>Los claims son ilimitados pero para estructuras propias como casas, huertos, granjas... cualquier cosa hecha o plantada por vosotros!'
+		},
+		{
+			title: 'Nada de robar y/o matar a otros jugadores',
+			description:
+				'Si alguien os ataca podéis defenderos y luego reportar al jugador que os atacó en <a href="https://discord.com/channels/726133117722820671/1174025705600843797" class="font-bold underline">este canal del servidor de Discord.</a>'
+		},
+		{
+			title: 'Nada de insultos y/o faltas de respeto',
+			description:
+				'La gente quiere disfrutar del juego y que alguien llegue a molestar no es de agrado para nadie así que respeta a otro jugadores igual que te gustaría que respetaran a ti.'
+		},
+		{
+			title: 'No cambiar el nombre de los teletransportes',
+			description:
+				'No cambiar el nombre de los teletransportes si están siendo usados por otra persona.'
+		},
+		{
+			title: 'Respeta el espacio de otros',
+			description:
+				'No construyas nada demasiado cerca de otros jugadores si su permiso, puede que quieran ampliar sus casa y luego no tienen espacio.'
+		},
+		{
+			title: 'Dejar un margen en el spawn',
+			description:
+				'Solo una pequeña zona del spawn está protegida pero eso no quiere decir que puedas construirte un casa a 10 bloques de este. Deja mínimo 500 bloques a cada lado de este para futuros proyectos!'
+		}
 	]
 };
---
+++ src/routes/rules/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { texts } from '$lib/config';
+</script>
+
+<!-- Wite your HTML here, this will replace the <slot /> tag on the +layout.svelte file. -->
+<!-- This will be surrounded by the header/nav and the footer. -->
+
+<!-- If you like how the page looks, you can just change the sentences on the config file located in src/lib/config.ts! -->
+
+<div
+	class="w-full max-w-7xl mx-auto py-4 pb-12 px-12 flex flex-col content-center justify-center gap-4 text-center"
+>
+	<h1 class="text-4xl font-bold mt-12 mb-6">{@html texts.RulesTitle}</h1>
+	<p class="text-lg">{@html texts.RulesDescription}</p>
+
+	<div class="mt-6 flex flex-col gap-8 lg:w-2/3 xl:w-1/2 m-auto">
+		{#each texts.Rules as rule}
+			<div class="flex flex-col gap-1">
+				<h2 class="text-xl font-bold">{@html rule.title}</h2>
+				<p>{@html rule.description}</p>
+			</div>
+		{/each}
+	</div>
+</div>
